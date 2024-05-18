bc06f5e1413239ea3c973b56cc932ccb329c9ab4
akifguldemir <akifguldemir@gmail.com>
2024-05-19 01:03:21 +0300

    New fields added

--- BlogApp/Entity/User.cs
+++ BlogApp/Entity/User.cs
@@ -9,6 +9,9 @@ namespace BlogApp.Entity
     {
         public int Id { get; set; }
         public string? UserName { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
 
         public string? Image { get; set; }
 
